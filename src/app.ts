4f794ec5321afce11f48ade39ef2d5c9e13e68eb
erik <erik@meucrediario.com.br>
2024-01-13 10:12:33 -0300

    Server config

---
+++ src/app.ts
@@ -0,0 +1,30 @@
+import { ExpressServer } from "./server/express";
+import express from "express";
+import { ConfigsApp } from "./types/ConfigsApp";
+import dotenv from "dotenv";
+
+dotenv.config()
+
+export const app = () => {
+    const config: ConfigsApp = {
+        port: Number(process.env.SERVER_PORT) || 3000
+    };
+
+    const expressInstance = express();
+
+    try {
+        const appServer = new ExpressServer(expressInstance, config.port)
+
+        appServer.httpServer.on('error', () => {
+            console.log('Server error')
+        })
+
+        appServer.httpServer.on('listening', () => {
+            console.log('Server started')
+        })
+    } catch (error) {
+        console.error('Error during server initialization:', error)
+    }
+}
+
+app()
--- src/server/express/server.ts
+++ src/server/express/server.ts
@@ -7,9 +7,23 @@ export class ExpressServer {
 
     constructor(server: Express, port: number) {
         this.setupStandardMiddleware(server)
+        //this.httpServer = this.startListen(server, port)
+    }
+
+    public startListen(server: Express, port: number): Server {
+        return server.listen(port)
     }
 
     private setupStandardMiddleware(server: Express) {
         server.use(bodyParser.json)
+
+        server.use((req, res, next) => {
+            res.header('Access-Control-Allow-Origin', '*');
+            res.header(
+                'Access-Control-Allow-Headers',
+                'Origin, X-Requested-With, Content-Type, Accept'
+            );
+            next();
+        })
     }
 }
